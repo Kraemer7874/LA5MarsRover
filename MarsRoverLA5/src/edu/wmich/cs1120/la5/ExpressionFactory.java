04d3964e566160e9c332012a9d8890aa7c787903
Kraemer7874 <jasmine.n.kraemer@wmich.edu>
2017-03-29 15:09:14 -0400

    Completed the ExpressionFactory class so now it can calculate the correct expression when given two values and the operation symbol

--- MarsRoverLA5/src/edu/wmich/cs1120/la5/ExpressionFactory.java
+++ MarsRoverLA5/src/edu/wmich/cs1120/la5/ExpressionFactory.java
@@ -4,8 +4,20 @@ public class ExpressionFactory {
 	// This method will create and return a proper object
 	//according to the arguments received
 	public static IExpression getExpression(char operator, int val1, int val2) {
-		
-		return null;
-	// fill your code here
+		IOperation op = null;
+		IExpression left = null;
+		IExpression right = null;
+		if (operator == '+'){
+			op = new Addition();
+			left = new Literal(val1);
+			right = new Literal(val2);
+		}
+		if (operator == '-'){
+			op = new Subtraction();
+			left = new Literal(val1);
+			right = new Literal(val2);
+		}
+		IExpression binary = new BinaryExpression(op, left, right);
+		return binary;
 	}
 }
