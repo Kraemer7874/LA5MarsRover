e3146474dabf991a57ab452c97ec75015b5a2583
Kraemer7874 <jasmine.n.kraemer@wmich.edu>
2017-03-29 21:38:43 -0400

    Updated Not completely the MapCreatorFromDat class

--- MarsRoverLA5/src/edu/wmich/cs1120/la5/MapCreatorFromDat.java
+++ MarsRoverLA5/src/edu/wmich/cs1120/la5/MapCreatorFromDat.java
@@ -1,25 +1,44 @@
 package edu.wmich.cs1120.la5;
 
+import java.io.DataInputStream;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Scanner;
+import edu.wmich.cs1120.la5.TerrainScanner;
 
 public class MapCreatorFromDat implements IMapCreator{
 
+	private TerrainScanner scanner;
 	@Override
 	public void scanTerrain(String fileName, int threshold) throws IOException {
-		// TODO Auto-generated method stub
+		FileInputStream fstream = new FileInputStream(fileName);
+		DataInputStream inputFile = new DataInputStream(fstream);
+		Area[][] terrain = new Area[10][10];
+		double basicEnergyCost;
+		double elevation;
+		double radiation;
+		for(int row = 0; row < 10; row++){
+			for(int col = 0; col < 10; col++){
+				basicEnergyCost = input.nextDouble();
+				elevation = input.nextDouble();
+				radiation = input.nextDouble();
+				input.nextLine();
+				terrain[row][col] = new Area(basicEnergyCost, elevation, radiation, threshold);
+			}
+		}
+		scanner = new TerrainScanner();
+		scanner.setTerrain(terrain);
 		
 	}
 
 	@Override
 	public TerrainScanner getScanner() {
-		// TODO Auto-generated method stub
-		return null;
+		return scanner;
 	}
 
 	@Override
 	public void setScanner(TerrainScanner scanner) {
-		// TODO Auto-generated method stub
-		
+		this.scanner = scanner;
 	}
 
 }
