485fdd50232c5f1c05694a6c142993c607227760
Kraemer7874 <jasmine.n.kraemer@wmich.edu>
2017-03-30 00:09:34 -0400

    Finished project...I think

--- MarsRoverLA5/src/edu/wmich/cs1120/la5/MapCreatorFromDat.java
+++ MarsRoverLA5/src/edu/wmich/cs1120/la5/MapCreatorFromDat.java
@@ -3,7 +3,9 @@ package edu.wmich.cs1120.la5;
 import java.io.DataInputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.RandomAccessFile;
 import java.util.Scanner;
+import edu.wmich.cs1120.la5.gui.MainWindow;
 import edu.wmich.cs1120.la5.TerrainScanner;
 
 public class MapCreatorFromDat implements IMapCreator{
@@ -11,22 +13,36 @@ public class MapCreatorFromDat implements IMapCreator{
 	private TerrainScanner scanner;
 	@Override
 	public void scanTerrain(String fileName, int threshold) throws IOException {
-		FileInputStream fstream = new FileInputStream(fileName);
-		DataInputStream inputFile = new DataInputStream(fstream);
+		RandomAccessFile inputFile = new RandomAccessFile(fileName, "r");
 		
 		Area[][] terrain = new Area[10][10];
 		double basicEnergyCost;
 		double elevation;
 		double radiation;
+		int point = 0;
+		//int row = 0;
+		//int col = 0;
+		int left = 0;
+		int right = 0;
+		char op = 'a';
+		
+		
 		for(int row = 0; row < 10; row++){
 			for(int col = 0; col < 10; col++){
-				basicEnergyCost = input.nextDouble();
-				elevation = input.nextDouble();
-				radiation = input.nextDouble();
-				input.nextLine();
-				terrain[row][col] = new Area(basicEnergyCost, elevation, radiation, threshold);
+			inputFile.seek(point * (3 * Double.BYTES + 1 * Character.BYTES + 2 * Integer.BYTES));
+			basicEnergyCost = inputFile.readDouble();
+			elevation = inputFile.readDouble();
+			radiation = inputFile.readDouble();
+			op = inputFile.readChar();
+			left = inputFile.readInt();
+			right = inputFile.readInt();
+			terrain[row][col] = new Area(basicEnergyCost, elevation, radiation, threshold);
+			IExpression num = ExpressionFactory.getExpression(op, left, right);
+			point = num.getValue();
+			
 			}
 		}
+	
 		scanner = new TerrainScanner();
 		scanner.setTerrain(terrain);
 		
